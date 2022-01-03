8e4c455c8a1b6a8a9bc472b06eed49e14fb66cec
szabinalovas <szabinaa.lovas@gmail.com>
2022-01-03 22:32:15 +0100

    create board

--- src/main/java/com/codecool/polishdraughts/App.java
+++ src/main/java/com/codecool/polishdraughts/App.java
@@ -1,8 +1,15 @@
 package com.codecool.polishdraughts;
 
+import com.codecool.polishdraughts.board.Board;
+
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-
+        System.out.println("Give me a number between 10 and 20:");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        Board board = new Board(size);
     }
 }
