07e56d774c17b15277c81c706e9789c4d2a1117b
Brigitta Arvai <arvaibrigitta@yahoo.co.uk>
2022-01-03 19:06:45 +0100

    add Game start method

--- src/main/java/com/codecool/polishdraughts/game/Game.java
+++ src/main/java/com/codecool/polishdraughts/game/Game.java
@@ -1,10 +1,20 @@
 package com.codecool.polishdraughts.game;
 
+import com.codecool.polishdraughts.board.Board;
+
 public class Game implements GameInterface{
+    private Board board;
+    private boolean isGameRunning;
 
     @Override
     public void start() {
-
+        int player = 1;
+        this.board = new Board();
+        this.isGameRunning = true;
+        while (isGameRunning) {
+            playRound();
+            player = player == 1 ? 2 : 1;
+        }
     }
 
     @Override
