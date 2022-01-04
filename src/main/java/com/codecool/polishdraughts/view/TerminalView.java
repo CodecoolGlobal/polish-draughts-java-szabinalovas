7c9c50dbc85b4545ec62935108e08de2fa472318
hojo84 <hojo84@gmail.com>
2022-01-04 18:16:53 +0100

    Implement method readInput()

--- src/main/java/com/codecool/polishdraughts/view/TerminalView.java
+++ src/main/java/com/codecool/polishdraughts/view/TerminalView.java
@@ -1,9 +1,19 @@
 package com.codecool.polishdraughts.view;
 
+import java.util.Scanner;
+
 public class TerminalView {
 
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+    }
+
+    public static String readInput(String prompt) {
+        System.out.print(prompt + " ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine().toUpperCase();
+        if (userInput.equals("QUIT")) System.exit(0);
+        return userInput;
     }
 }
