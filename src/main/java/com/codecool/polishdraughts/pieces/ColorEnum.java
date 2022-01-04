6685593edcb897aaf7c29af832cfe42f1c1ef4b6
istvanolah <istvanolah@freemail.hu>
2022-01-04 18:59:39 +0100

    Pawn, ColorEnum repaired

--- src/main/java/com/codecool/polishdraughts/pieces/ColorEnum.java
+++ src/main/java/com/codecool/polishdraughts/pieces/ColorEnum.java
@@ -1,5 +1,13 @@
 package com.codecool.polishdraughts.pieces;
-
 public enum ColorEnum {
-    BLACK, WHITE
+    BLACK(Pawn.ANSI_WHITE_BACKGROUND + Pawn.ANSI_BLACK +"O"+Pawn.ANSI_BLACK_BACKGROUND+Pawn.ANSI_RESET), WHITE("O");
+    private String pownChar;
+
+    ColorEnum(String pChar) {
+        this.pownChar = pChar;
+    }
+
+    public String getPownChar() {
+        return pownChar;
+    }
 }
